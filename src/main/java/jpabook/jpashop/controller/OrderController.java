350d66d7dc153f4497e51cbede5e880933162bee
ppparkta <pppppark2@gmail.com>
2023-12-20 16:57:11 +0900

    Revert "feat: chapter7 주문 취소 기능 추가"
    This reverts commit d662d933837188d9b3a0b68ab02f50f4998703a3.

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -10,7 +10,10 @@ import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -45,11 +48,5 @@ public class OrderController {
         model.addAttribute("orders", orders);
 
         return "order/orderList";
-    }
-
-    @PostMapping("/orders/{orderId}/cancel")
-    public String cancelOrder(@PathVariable("orderId")Long orderId){
-        orderService.cancelOrder(orderId);
-        return "redirect:/orders";
     }
 }
