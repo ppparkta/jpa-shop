6d85c7d8edbbf40eaa2c1b65e6e7a54a1fc886e5
ppparkta <pppppark2@gmail.com>
2024-01-05 17:49:22 +0900

    feat: 컬렉션 DTO로 직접 조회할 때 성능 최적화(컬렉션조회 최적화, 플랫데이터)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -7,12 +7,12 @@ import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +21,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
+
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
-    /***
-     * 이전 강의 설명을 안 따라했더니 실행이 안 됨...ㅋㅋㅋㅋ
-     * @return
-     */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAll(new OrderSearch());
@@ -50,7 +50,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAll(new OrderSearch());
         List<OrderDto> collect = orders.stream()
                 .map(order -> new OrderDto(order))
-                .collect(Collectors.toList());
+                .collect(toList());
         return collect;
     }
 
@@ -70,7 +70,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
             List<OrderItemDto> orderItemDtos = order.getOrderItems().stream()
                     .map(oi-> new OrderItemDto(oi))
-                    .collect(Collectors.toList());
+                    .collect(toList());
             orderItems = orderItemDtos;
         }
     }
@@ -96,7 +96,7 @@ public class OrderApiController {
         }
         List<OrderDto> collect = orders.stream()
                 .map(order -> new OrderDto(order))
-                .collect(Collectors.toList());
+                .collect(toList());
         return collect;
     }
 
@@ -108,8 +108,29 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o->new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(Collectors.groupingBy(o->new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(),o.getAddress()),
+                        mapping(o->new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e->new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 }
